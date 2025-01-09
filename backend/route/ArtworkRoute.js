cb31d87c9977d29f96dcf5d11efd0ffc8ac8f191
Sarthak Singh <mzppsarthakjvb@gmail.com>
2025-01-09 20:22:03 +0530

    Add Delete button for all ArtWorks

--- backend/route/ArtworkRoute.js
+++ backend/route/ArtworkRoute.js
@@ -15,13 +15,25 @@ router.post("/", async (req, res) => {
 
 // get all art work
 
-router.get("/", async(req, res) => {
-    try{
-        const artworks = await ArtWork.find();
-        res.json(artworks);
-    }catch(err){
-        res.status(500).json({Error: err.message});
-    }
+router.get("/", async (req, res) => {
+  try {
+    const artworks = await ArtWork.find();
+    res.json(artworks);
+  } catch (err) {
+    res.status(500).json({ Error: err.message });
+  }
+});
+
+// delete art
+router.delete("/:id", async (req, res) => {
+  try {
+    const ArtId = req.params.id;
+    await ArtWork.findByIdAndDelete(ArtId);
+    res.status(200).json({ Message: "Artwork Deleted Successfully!." });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ Error: error.message });
+  }
 });
 
 module.exports = router;
--- client/src/components/ArtGallery.js
+++ client/src/components/ArtGallery.js
@@ -1,37 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const ArtworkGallery = () => {
-    const [artworks, setArtworks] = useState([]);
+  const [artworks, setArtworks] = useState([]);
 
-    useEffect(() => {
-        const fetchArtworks = async () => {
-            const response = await axios.get('http://localhost:9000/api/artworks');
-            setArtworks(response.data);
-        };
-        fetchArtworks();
-    }, []);
+  useEffect(() => {
+    const fetchArtworks = async () => {
+      const response = await axios.get("http://localhost:9000/api/artworks");
+      setArtworks(response.data);
+    };
+    fetchArtworks();
+  }, []);
 
-    return (
-        <div>
-            <h1>Gallery</h1>
-            <div style={{ display: 'flex', flexWrap: 'wrap'}} >
-                {artworks.map((artwork) => (
-                    <div key={artwork._id} style={{ margin: 10 }}>
-                        <h3>{artwork.title}</h3>
-                        <img src={artwork.image} alt={artwork.title} style={{ width: 200 ,border: "2px solid black"}} />
-                    </div>
-                ))}
+  const handleDelete = async(id) => {
+    try {
+      await axios.delete(`http://localhost:9000/api/artworks/${id}`);
+      setArtworks((prevArtWork) =>
+        prevArtWork.filter((artworks) => artworks._id !== id)
+      );
+    } catch (err) {
+      console.error("Error deleting artwork:", err);
+    }
+  };
+
+  return (
+    <>
+      {" "}
+      <div>
+        <h1>Gallery</h1>
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          {artworks.map((artwork) => (
+            <div key={artwork._id} style={{ margin: 10 }}>
+              <h3>{artwork.title}</h3>
+              <img
+                src={artwork.image}
+                alt={artwork.title}
+                style={{ width: 200, border: "2px solid black" }}
+              />
+              <div>
+                <button onClick={()=> handleDelete(artwork._id)}
+                  style={{ width: "100px", backgroundColor: "#092902" }}
+                >
+                  Delete
+                </button>
+              </div>
             </div>
-            <a
+          ))}
+        </div>
+        <a
           href="http://localhost:3000"
           target="_parent"
           rel="noopener noreferrer"
         >
           <button>Make More Gallery</button>
         </a>
-        </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default ArtworkGallery;
